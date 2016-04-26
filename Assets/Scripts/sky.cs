b59d0556bb8f23fc6b57d5d6ff92e2a6aaa84d22
Sungkeun Choi <soliven25@gmail.com>
2016-04-26 16:38:22 +0900

    add alpha value of background

---
+++ Assets/Scripts/sky.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sky : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer spr = GetComponent<SpriteRenderer>();
+		Color color = spr.color;
+		color.a = 0.7f;
+		spr.color = color;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
