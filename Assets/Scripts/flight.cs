790966b533aa4a92d975943af10efcdef10536f8
Sungkeun Choi <soliven25@gmail.com>
2016-04-27 12:26:30 +0900

    add bullet fire sound

--- Assets/Scripts/flight.cs
+++ Assets/Scripts/flight.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class flight : MonoBehaviour {
 
 	// Use this for initialization
 	public float speed;
 	public GameObject bulletPrefab;
+	
+	public AudioClip fireSound;
+	AudioSource audio;
 	void Start () {
 		speed = 0.06F;
+		audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +24,8 @@ public class flight : MonoBehaviour {
 		transform.Translate(Vector3.up*speed*upDown, Space.World);
 		
 		if(Input.GetKeyDown("space")){
+			audio.clip = fireSound;
+			audio.Play();
 			Instantiate(bulletPrefab, transform.position, transform.rotation);
 			//Debug.Log("space pressed");
 		}		
