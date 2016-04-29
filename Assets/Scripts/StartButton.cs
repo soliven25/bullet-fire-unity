ea7414b0cb1ff5ed0d46258d0de20b6164e7bdc6
Sungkeun Choi <soliven25@gmail.com>
2016-04-29 11:34:49 +0900

    add button & scene transition

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+	public void OnClick(){
+		Debug.Log("StartButton On Click");
+		Application.LoadLevel("eye_practice");
+	}
+	
+	public void OnOver(){
+		Debug.Log("StartButton On Over");	
+	}
+	
+	public void OnExit(){
+		Debug.Log("StartButton On Exit");
+	}
+}
