1fb88ba570e2743b8181631ef5f686d1d5fd2fe9
Sungkeun Choi <soliven25@gmail.com>
2016-04-29 16:04:15 +0900

    change Edit --> Project Setting -->Editor

---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFlow : MonoBehaviour {
+
+	// Use this for initialization
+	public void OnClick(){
+		Debug.Log("GameFlow On Click");
+		Application.LoadLevel("UI");
+	}
+	
+	public void OnOver(){
+		Debug.Log("GameFlow On Over");	
+	}
+	
+	public void OnExit(){
+		Debug.Log("GameFlow On Exit");
+	}
+}
